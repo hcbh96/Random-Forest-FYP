57d243586a9217e76aa46aee50eb0b725f69bd21
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2019-12-22 11:04:11 +0000

    roc_auc now a callable function

--- DataCleaning/roc_auc.py
+++ DataCleaning/roc_auc.py
@@ -16,39 +16,38 @@ from evaluate_model import evaluate_model
 from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 
-# set a random seed to ensure reproducible runs
-RSEED = 50
-# read in dataframe
-dtfm=pd.read_excel('cleaned_data.xlsx', sheet_name='Sheet1')
-# remove columns not being used
-dtfm = dtfm.drop(columns=['ORDEM','DATA','AMOSTRA','REPLICATA','ANIMAL','PARTIDA','CELLS_COUNT', 'CLIV'])
-#describe Blast_D8 output vars
-desc_blast = dtfm["BLAST_D8"].describe()
-print("Describe Output Vars: \n {}".format(desc_blast))
-# create a linspace of 10 points between mu-2sd and mu+2sd
-mean = desc_blast['mean']
-std = desc_blast['std']
-threasholds = np.linspace(mean - 1.5 * std, mean + 1.5 * std, 100)
-print("Threasholds at which we'll calculte ROC_AUC: \n {}".format(threasholds))
-# ROC_AUC array
-roc_auc_arr = []
+
+def roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=False):
+    """Returns the roc_auc for an optimised Random Forest
+    Trained and tested over a passed in dataset
+
+    USGAE: roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=False)
+
+    INPUTS:
+        dtfm: dataframe to train and test model on
+        labels_col: column title containing the classification flag
+
+    **Optional** (default)
+        test_size: (0.3) proportion of data set to use for testing
+        random_state: (np.random) seed used by the random number generator
+        logger: (False) wheather to log outputs or not
 
 
-#create loop
-for threashold in threasholds:
-    print("Optimising a RF for threashold: {}".format(threashold))
-    dtfm_temp = dtfm.copy()# make copy of dtfm
-    # update blast labels to sit above or below threashold
-    dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] >= threashold, other=0)
-    dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] < threashold, other=1)
+    OUTPUT:
+        roc_auc: (scalar) Area under the receiver operating
+            charachteristic curve
+    """
+    dtfm_labels = dtfm.pop(labels_col)
+    # separate the labels from the data
+    labels = np.array(dtfm_labels)
+    print(dtfm.head())
+
     # print value counts so we can see how split affects data
-    print("Blast_D8 value counts:\n {}".format(dtfm_temp['BLAST_D8'].value_counts()))
-    labels = np.array(dtfm_temp.pop('BLAST_D8'))#extract labels
+    if logger:
+        print("Output value count:\n {}".format(dtfm_labels.value_counts()))
 
-
-    # split data into train and test sets 30% test
-    train, test, train_labels, test_labels = train_test_split(dtfm_temp,
-            labels, stratify = labels, test_size = 0.3, random_state = RSEED)
+    # split data into train and test sets split% test
+    train, test, train_labels, test_labels = train_test_split(dtfm,labels, stratify = labels, test_size = test_size, random_state=random_state)
 
     #imputation of missing values
     train = train.fillna(train.mean())
@@ -63,19 +62,19 @@ for threashold in threasholds:
         'min_samples_split': [2, 5, 10],
         'bootstrap': [True, False]
     }
-
     # Estimator for use in random search
-    estimator = RandomForestClassifier(random_state = RSEED)
-
+    estimator = RandomForestClassifier(random_state = random_state)
     # Create the random search model
     rs = RandomizedSearchCV(estimator, param_grid, n_jobs = -1,
                          scoring = 'roc_auc', cv = 3,
-                         n_iter = 10, verbose = 1, random_state=RSEED)
-
+                         n_iter = 10, verbose = 1,
+                         random_state=random_state)
     # Fit
     rs.fit(train, train_labels)
 
-    print("Best params:\n{}".format(rs.best_params_))
+    # print result
+    if logger:
+        print("Best params:\n{}".format(rs.best_params_))
 
     # Try using the best model
     best_model = rs.best_estimator_
@@ -87,15 +86,43 @@ for threashold in threasholds:
     rf_probs = best_model.predict_proba(test)[:, 1]
 
     # calculate roc_auc and append to roc_auc_arr
-    score = roc_auc_score(test_labels, rf_probs)
-    roc_auc_arr.append(score)
-    print("ROC AUC Score=",score)
-    #evaluate_model(rf_predictions, rf_probs, train_rf_predictions, train_rf_probs,test_labels, train_labels, title='Optimised Forest ROC Curve')
+    auc = roc_auc_score(test_labels, rf_probs)
+    if logger:
+        print("ROC AUC Score=",auc)
+
+    return auc
+
+if __name__ == "__main__":
+    # read in dataframe
+    dtfm=pd.read_excel('cleaned_data.xlsx', sheet_name='Sheet1')
+    # remove columns not being used
+    dtfm = dtfm.drop(columns=['ORDEM','DATA','AMOSTRA','REPLICATA','ANIMAL','PARTIDA','CELLS_COUNT', 'CLIV'])
+    #describe Blast_D8 output vars
+    desc_blast = dtfm["BLAST_D8"].describe()
+    # create a linspace of 10 points between mu-2sd and mu+2sd
+    mean = desc_blast['mean']
+    std = desc_blast['std']
+    threasholds = np.linspace(mean - 1.5 * std, mean + 1.5 * std, 10)
+    print("Threasholds at which we'll calculte ROC_AUC: \n {}".format(threasholds))
+    # ROC_AUC array
+    roc_auc_arr = []
+
+    #create loop
+    for threashold in threasholds:
+        print("Optimising a RF for threashold: {}".format(threashold))
+        dtfm_temp = dtfm.copy()# make copy of dtfm
+        # update blast labels to sit above or below threashold
+        dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] >= threashold, other=0)
+        dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] < threashold, other=1)
+
+        # run roc_auc func
+        score = roc_auc(dtfm_temp, 'BLAST_D8', random_state=50, logger=True)
+        roc_auc_arr.append(score)
 
 
-# plot roc_auc curve against threashold values
-plt.plot(threasholds, roc_auc_arr)
-plt.ylabel('ROC_AUC')
-plt.xlabel('Threashold')
-plt.ylim(0,1)
-plt.show()
+    # plot roc_auc curve against threashold values
+    plt.plot(threasholds, roc_auc_arr)
+    plt.ylabel('ROC_AUC')
+    plt.xlabel('Threashold')
+    plt.ylim(0,1)
+    plt.show()
