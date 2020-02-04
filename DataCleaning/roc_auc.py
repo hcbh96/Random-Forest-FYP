78ea502868e79cc212d36c778c22acddf1b089c7
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2020-02-04 09:19:20 +0000

    gradually changing colour for reduced var plot now working

--- DataCleaning/roc_auc.py
+++ DataCleaning/roc_auc.py
@@ -12,7 +12,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
-from evaluate_model import evaluate_model
 from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -21,7 +20,7 @@ def roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=Fals
     """Returns the roc_auc for an optimised Random Forest
     Trained and tested over a passed in dataset
 
-    USGAE: roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=False)
+    USGAE: roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=logger)
 
     INPUTS:
         dtfm: dataframe to train and test model on
@@ -98,23 +97,15 @@ def roc_auc(dtfm, labels_col, test_size=0.3, random_state=np.random, logger=Fals
                 'importance': best_model.feature_importances_})
 
     if logger:
-        print("Features of most importance in RF:\n{}".format(fi_model.head(3)))
-        print("Features of least importance in RF: \n{}".format(fi_model.tail(3)))
+        print("Features importance in RF:\n{}".format(fi_model.sort_values('importance', 0, ascending=True)))
 
     return [auc, fi_model]
 
-if __name__ == "__main__":
-    # read in dataframe
-    dtfm=pd.read_excel('cleaned_data.xlsx', sheet_name='Sheet1', index_col=0)
-    # remove columns not being used
-    dtfm = dtfm.drop(columns=['ORDEM','DATA','AMOSTRA','REPLICATA','ANIMAL','PARTIDA','CELLS_COUNT', 'CLIV'])
-    #describe Blast_D8 output vars
-    desc_blast = dtfm["BLAST_D8"].describe()
-    # create a linspace of 10 points between mu-2sd and mu+2sd
-    mean = desc_blast['mean']
-    std = desc_blast['std']
-    threasholds = np.linspace(mean - 1.5 * std, mean + 1.5 * std, 5)
-    print("Threasholds at which we'll calculte ROC_AUC: \n {}".format(threasholds))
+
+def test_threasholds(threasholds, dtfm, dep_key='BLAST_D8', random_state=50, logger=False):
+    if logger== True:
+        print("Threasholds at which we'll calculte ROC_AUC: \n {}".format(threasholds))
+
     # ROC_AUC array
     roc_auc_arr = []
     fi_dtfm = pd.DataFrame()
@@ -122,15 +113,17 @@ if __name__ == "__main__":
 
     #create loop
     for threashold in threasholds:
-        print("Optimising a RF for threashold: {}".format(threashold))
+        if logger == True:
+            print("Optimising a RF for threashold: {}".format(threashold))
         dtfm_temp = dtfm.copy()# make copy of dtfm
         # update blast labels to sit above or below threashold
-        dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] >= threashold, other=0)
-        dtfm_temp['BLAST_D8'] = dtfm_temp['BLAST_D8'].where(dtfm['BLAST_D8'] < threashold, other=1)
+        dtfm_temp[dep_key] = dtfm_temp[dep_key].where(dtfm[dep_key] >= threashold, other=0)
+        dtfm_temp[dep_key] = dtfm_temp[dep_key].where(dtfm[dep_key] < threashold, other=1)
 
         # run roc_auc func
-        [score, fi] = roc_auc(dtfm_temp, 'BLAST_D8', random_state=50, logger=False)
+        [score, fi] = roc_auc(dtfm_temp, dep_key, random_state=random_state, logger=logger)
 
+        # create first index
         if dtfm_index == 0:
             fi_dtfm.insert(0, 'feature', fi.feature)
 
@@ -138,33 +131,153 @@ if __name__ == "__main__":
         dtfm_index += 1
         roc_auc_arr.append(score)
 
-
-
-    # plot roc_auc curve against threashold values
-    # plt.plot(threasholds, roc_auc_arr)
-    # plt.ylabel('ROC_AUC')
-    # plt.xlabel('Threashold')
-    # plt.ylim(0,1)
-    # plt.show()
-
     # redefine index
     fi_dtfm = fi_dtfm.set_index('feature')
     fi_dtfm['mean'] = fi_dtfm.mean(axis=1)
     fi_dtfm = fi_dtfm.sort_values('mean', 0, ascending=True)
     fi_dtfm = fi_dtfm.transpose()
 
+    return [
+            roc_auc_arr,
+            fi_dtfm,
+           ]
+
+
+
+
+if __name__ == "__main__":
+    # define what to run
+    logger = True
+    plot_roc_auc = False
+    plot_boxplot = False
+    plot_heatmap = False
+    high_threasholds = True
+    low_threasholds = True
+    m_v_i = False
+    n_threasholds = 10
+
+    #cols to drop
+    discrete_cols = ['ORDEM','DATA','AMOSTRA','REPLICATA','ANIMAL','PARTIDA',   'CELLS_COUNT', 'CLIV']
+    discrete_cols = discrete_cols + ['SUB_1_RP','SUB_2_H','SUB_3_LS','SUB_4_LP']
+    motility_cols = ['VAP','VSL','VCL','ALH','BCF','STR','LIN','MOTILE_PCT',
+'PROGRESSIVE_PCT','RAPID_PCT','MEDIUM_PCT','SLOW_PCT','STATIC_PCT']
+    integrity_cols = ['AI','PI','ALTO','FRAG_CRO',
+'MOT_PRE','MOT_POS','CONC_CAMARA','VF','AD']
+
+    # read in dataframe
+    dtfm=pd.read_excel('cleaned_data.xlsx', sheet_name='Sheet1', index_col=0)
+
+    # remove columns not being used
+    dtfm = dtfm.drop(columns=discrete_cols)
+
+    # create a linspace of 10 points between mu-2sd and mu+2sd
+    desc_blast = dtfm["BLAST_D8"].describe()
+    mean = desc_blast['mean']
+    std = desc_blast['std']
+    threasholds = np.linspace(mean - 1.5 * std, mean + 1.5 * std, n_threasholds)
+
+    # test threashold norm
+    [roc_auc_arr, fi_dtfm] = test_threasholds(threasholds, dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
+
+    # plot roc_auc curve against threashold values
+    if plot_roc_auc == True:
+        plt.plot(threasholds, roc_auc_arr)
+        plt.ylabel('ROC_AUC')
+        plt.xlabel('Threashold')
+        plt.ylim(0,1)
+        plt.show()
+
     # plot box plot of most important to least important vars
-    boxplot = fi_dtfm.drop('mean').boxplot(rot=90)
-    plt.ylabel('Blastocyst Threashold')
-    plt.xlabel('Feature')
-    plt.tight_layout()
-    plt.show()
+    if plot_boxplot == True:
+        boxplot = fi_dtfm.drop('mean').boxplot(rot=90)
+        plt.ylabel('Blastocyst Threashold')
+        plt.xlabel('Feature')
+        plt.tight_layout()
+        plt.show()
 
     # plot heatmap of most to least important vars
-    sns.heatmap(fi_dtfm)
-    plt.xlabel('Feature')
-    plt.ylabel('Blastocyt Threashold')
-    plt.tight_layout()
-    #plt.show()
+    if plot_heatmap:
+        sns.heatmap(fi_dtfm)
+        plt.xlabel('Feature')
+        plt.ylabel('Blastocyt Threashold')
+        plt.tight_layout()
+        plt.show()
+
+    imp_dtfm = dtfm.copy()
+    # run roc_auc multiple times
+    if high_threasholds == True:
+        drop_cols = ['Basis']
+        # set up loop
+        for i in range(len(dtfm.columns) - 1):
+            RED = i/(len(dtfm.columns) + 1)
+            GREEN = 0
+            BLUE = 1 - i/(len(dtfm.columns) + 1)
+            print("colors RED:{}, GREEN:{}, BLUE:{}".format(RED, GREEN, BLUE))
+            # run roc_auc
+            [roc_auc_arr, fi_dtfm] = test_threasholds(threasholds, imp_dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
+
+            # remove worst variable from dataframe
+            drop_col = fi_dtfm.columns[0]
+            drop_cols.append('{}:{}'.format(i,drop_col))
+            print("Cols left: {}".format(len(fi_dtfm.columns)))
+            print('Dropped Column {}: {}'.format(i,drop_col))
+            imp_dtfm = imp_dtfm.drop(columns=[drop_col])
+            # save roc_auc
+            plt.plot(threasholds, roc_auc_arr, color=[RED, GREEN, BLUE])
+
+        # plot roc_auc for various for various threasholds
+        plt.ylabel('ROC_AUC')
+        plt.xlabel('Threashold')
+        plt.ylim(0,1)
+        plt.legend(drop_cols, ncol=4)
+        plt.show()
 
 
+    imp_dtfm = dtfm.copy()
+    # run roc_auc multiple times
+    if low_threasholds == True:
+        drop_cols = ['Basis']
+        # set up loop
+        for i in range(len(dtfm.columns) - 1):
+            RED = i/(len(dtfm.columns)+1)
+            GREEN = 0
+            BLUE = 1 - i/(len(dtfm.columns) + 1)
+            print("colors RED:{}, GREEN:{}, BLUE:{}".format(RED, GREEN, BLUE))
+            # run roc_auc
+            [roc_auc_arr, fi_dtfm] = test_threasholds(threasholds, imp_dtfm,             dep_key='BLAST_D8', random_state=50, logger=logger)
+
+            # remove worst variable from dataframe
+            drop_col = fi_dtfm.columns[-1]
+            drop_cols.append('{}:{}'.format(i,drop_col))
+            print("Cols left: {}".format(len(fi_dtfm.columns)))
+            print('Dropped Column {}: {}'.format(i,drop_col))
+            imp_dtfm = imp_dtfm.drop(columns=[drop_col])
+            # save roc_auc
+            plt.plot(threasholds, roc_auc_arr, color=[RED, GREEN, BLUE])
+
+        # plot roc_auc for various for various threasholds
+        plt.ylabel('ROC_AUC')
+        plt.xlabel('Threashold')
+        plt.ylim(0,1)
+        plt.legend(drop_cols, ncol=4)
+        plt.show()
+
+
+    if m_v_i == True:
+        # plot for integrity vs motility
+        integrity_dtfm = dtfm.drop(columns=motility_cols)
+        motility_dtfm = dtfm.drop(columns=integrity_cols)
+
+        # test integrity
+        [roc_auc_arr, fi_dtfm] = test_threasholds(threasholds, integrity_dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
+        plt.plot(threasholds, roc_auc_arr)
+
+        [roc_auc_arr, fi_dtfm] = test_threasholds(threasholds, motility_dtfm,dep_key='BLAST_D8', random_state=50, logger=logger)
+        plt.plot(threasholds, roc_auc_arr)
+
+        #plot integrity vs motility
+        plt.ylabel('ROC_AUC')
+        plt.xlabel('Threashold')
+        plt.ylim(0,1)
+        plt.legend(['Integrity','Motility'])
+        plt.show()
