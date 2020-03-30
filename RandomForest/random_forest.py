7f74e7eab1d48277c70a6a960a7eb48623514bcc
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2020-03-30 19:13:36 +0200

    Changed file structure

--- RandomForest/random_forest.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-
-# Read in data
-dtfm=pd.read_excel('initial_data.xlsx', sheet_name='BD_Research_Fapesp_final', header=1,usecols=[4,5,32,33,34])
-
-print(dtfm.head())
