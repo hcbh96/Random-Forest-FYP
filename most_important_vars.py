3450c77742b54b6746efee049c2e50e7514c1a34
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2020-04-13 15:38:31 +0200

    base case tests now running

--- most_important_vars.py
+++ most_important_vars.py
@@ -6,14 +6,15 @@ if __name__ == "__main__":
     from test_threasholds import test_threasholds
 
     # define what to run
-    logger = False
+    logger = True
     save_fig = False
-    tree_plot_roc_auc = True
-    tree_plot_boxplot = True
-    tree_plot_heatmap = True
-    rf_plot_roc_auc = True
-    rf_plot_boxplot = True
-    rf_plot_heatmap = True
+    tree_plot_roc_auc = False
+    tree_plot_boxplot = False
+    tree_plot_heatmap = False
+    rf_plot_roc_auc = False
+    rf_plot_boxplot = False
+    rf_plot_heatmap = False
+    base_case_predictions=True
     n_threasholds = 30
 
 
@@ -37,18 +38,19 @@ if __name__ == "__main__":
     std = desc_blast['std']
     threasholds = np.linspace(mean - 1.5 * std, mean + 1.5 * std, n_threasholds)
 
-    # test threashold RF
-    [
-        fi_dtfm,
-        roc_auc_arr,
-        roc_auc_train_arr,
-        precision_arr,
-        precision_train_arr,
-        recall_arr,
-        recall_train_arr,
-        accuracy_arr,
-        accuracy_train_arr,
-    ] = test_threasholds(threasholds, dtfm, classifier=DecisionTreeClassifier, dep_key='BLAST_D8', random_state=50, logger=logger)
+    if tree_plot_roc_auc or tree_plot_boxplot or tree_plot_heatmap:
+        # test threashold RF
+        [
+            fi_dtfm,
+            roc_auc_arr,
+            roc_auc_train_arr,
+            precision_arr,
+            precision_train_arr,
+            recall_arr,
+            recall_train_arr,
+            accuracy_arr,
+            accuracy_train_arr,
+        ] = test_threasholds(threasholds, dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
 
     # plot roc_auc curve against threashold values
     if tree_plot_roc_auc == True:
@@ -82,18 +84,19 @@ if __name__ == "__main__":
         else:
             plt.show()
 
-    # test threashold RF
-    [
-        fi_dtfm,
-        roc_auc_arr,
-        roc_auc_train_arr,
-        precision_arr,
-        precision_train_arr,
-        recall_arr,
-        recall_train_arr,
-        accuracy_arr,
-        accuracy_train_arr,
-    ] = test_threasholds(threasholds, dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
+    if rf_plot_roc_auc or rf_plot_boxplot or rf_plot_heatmap:
+        # test threashold RF
+        [
+            fi_dtfm,
+            roc_auc_arr,
+            roc_auc_train_arr,
+            precision_arr,
+            precision_train_arr,
+            recall_arr,
+            recall_train_arr,
+            accuracy_arr,
+            accuracy_train_arr,
+        ] = test_threasholds(threasholds, dtfm, dep_key='BLAST_D8', random_state=50, logger=logger)
 
     # plot roc_auc curve against threashold values
     if rf_plot_roc_auc == True:
@@ -128,4 +131,69 @@ if __name__ == "__main__":
             plt.show()
 
 
+    if base_case_predictions:
+        # this can be used to ensure that on a random output the classifier does not work
+        # therefore acting as a proof that the results are real
+        dtfm['randNumCol'] = np.random.randint(1, 100, dtfm.shape[0])
+        dtfm = dtfm.drop(columns=['BLAST_D8'])
+        # test threashold RF
+        [
+            c_fi_dtfm,
+            c_roc_auc_arr,
+            c_roc_auc_train_arr,
+            c_precision_arr,
+            c_precision_train_arr,
+            c_recall_arr,
+            c_recall_train_arr,
+            c_accuracy_arr,
+            c_accuracy_train_arr,
+        ] = test_threasholds(threasholds, dtfm, classifier=DecisionTreeClassifier, dep_key='randNumCol', random_state=50, logger=logger)
 
+
+        # test threashold RF
+        [
+            fi_dtfm,
+            roc_auc_arr,
+            roc_auc_train_arr,
+            precision_arr,
+            precision_train_arr,
+            recall_arr,
+            recall_train_arr,
+            accuracy_arr,
+            accuracy_train_arr,
+        ] = test_threasholds(threasholds, dtfm, dep_key='randNumCol', random_state=50, logger=logger)
+
+
+        # print mean and SD
+        if logger:
+            print("Accuracy Mean: {}, SD: {}".format(np.mean(accuracy_arr), np.std(accuracy_arr)))
+            print("Recall Mean: {}, SD: {}".format(np.mean(recall_arr), np.std(recall_arr)))
+            print("Precision Mean: {}, SD: {}".format(np.mean(precision_arr), np.std(precision_arr)))
+            print("ROC AUC Mean: {}, SD: {}".format(np.mean(roc_auc_arr),np.std(roc_auc_arr)))
+
+        # print mean and SD
+        if logger:
+            print("Accuracy Mean: {}, SD: {}".format(np.mean(c_accuracy_arr), np.std(c_accuracy_arr)))
+            print("Recall Mean: {}, SD: {}".format(np.mean(c_recall_arr), np.std(c_recall_arr)))
+            print("Precision Mean: {}, SD: {}".format(np.mean(c_precision_arr), np.std(c_precision_arr)))
+            print("ROC AUC Mean: {}, SD: {}".format(np.mean(c_roc_auc_arr),np.std(c_roc_auc_arr)))
+
+        # plot roc_auc for various for various threasholds
+        d = {
+                "RF":roc_auc_arr,
+                "CART": c_roc_auc_arr,
+            }
+
+        if logger:
+            print("Metrics:\n{}".format(d))
+
+        plot_dtfm = pd.DataFrame(data=d)
+        plot_dtfm = plot_dtfm.boxplot()
+        plt.ylabel('Method')
+        plt.xlabel('AUC')
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        if save_fig:
+            plt.savefig('classifiers_on_random_set')
+        else:
+            plt.show()
